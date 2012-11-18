c5a63a5149537bbacfd76731ff95f384ff267adc
Venkatesh Srinivas <me@ip-10-249-9-240.us-west-2.compute.internal>
2012-11-18 09:21:09 +0000

    db-stuff

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 OBJS=fftw.o workqueue.o
 
-sftw: libw.a
-	cc -g -o sftw sftw.c libw.a -pthread -lrt
+sftw: libw.a sftw.c $(OBJS)
+	$(CC) -g -o sftw sftw.c libw.a -pthread -lrt -I$(HOME)/db.1.85/include $(HOME)/db.1.85/PORT/linux/libdb.a
 
 libw.a: $(OBJS)
 	ar rcs libw.a $(OBJS)
 
+dumpdb: 
+	$(CC) -g -o dumpdb dumpdb.c -I$(HOME)/db.1.85/include $(HOME)/db.1.85/PORT/linux/libdb.a 
+
 clean:
-	rm -f $(OBJS) libw.a
+	rm -f $(OBJS) libw.a sftw
--- sftw.c
+++ sftw.c
@@ -3,6 +3,12 @@
 #include <strings.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <db.h>
 #include "workqueue.h"
 
 char *cmd = "s3cmd";
@@ -11,13 +17,100 @@ struct item {
 	char *path;
 };
 
+struct db {
+	pthread_mutex_t		db_mtx;
+	/* URL to Score */
+	DB			*hashmap;
+};
+
+static struct db sysdb;
+
+static void setup_db() {
+	sysdb.hashmap = dbopen("hashmap.dat", O_CREAT|O_RDWR, 0777, DB_BTREE,
+			       NULL);
+}
+
+static void teardown_db() {
+	sysdb.hashmap->sync(sysdb.hashmap, 0);
+	sysdb.hashmap->close(sysdb.hashmap);
+}
+
+static void store_db(char *url, unsigned long long size, char *score) {
+	DBT key, value;
+	key.data = url;
+	key.size = strlen(url);
+	value.data = score;
+	value.size = strlen(score);
+	assert(value.size == 32);
+	pthread_mutex_lock(&sysdb.db_mtx);
+	do {
+		sysdb.hashmap->put(sysdb.hashmap, &key, &value, 0);
+		sysdb.hashmap->sync(sysdb.hashmap, 0);
+	} while(0);
+	pthread_mutex_unlock(&sysdb.db_mtx);
+}
+
 void process_info(struct item *it, struct barrier *bar) {
 	int s;
 	char *xcmd;
+	FILE *fp;
+	int i;
+	char linebuf[512];
+	char *cp;
+	char *p;
+
+	unsigned long long size;
+	char *url;
+	char *score;
+	
+	url=score=NULL;
 	s = strlen(cmd) + strlen(it->path) + 16;
 	xcmd = calloc(1, s);
 	snprintf(xcmd, s, "%s info %s", cmd, it->path);
+/*
 	system(xcmd);
+ */
+
+	fp = popen(xcmd, "r");
+	for (i = 0;; i++) {
+		bzero(linebuf, sizeof(linebuf));
+		cp = fgets(linebuf, sizeof(linebuf), fp);
+		if (cp == NULL)
+			break;
+
+		p = index(linebuf, ':');
+		switch (i) {
+		case 0: /* URL */
+			p = strstr(linebuf, "(object)");
+			p--;
+			url = strndup(linebuf, p-linebuf);
+			break;
+		case 1: /* size */
+			p++;
+			size = atoll(p);
+			break;
+		case 2: /* mtime */
+			break;
+		case 3: /* mime */
+			break;
+		case 4: /* md5 */
+			p++;
+			while (isspace(*p))
+				p++;
+			score = strndup(p, 32);
+			break;
+		case 5: /* acl */
+			break;
+		}
+	}
+	/* Got a full record; in the future, abort/retry XXX */
+	assert(i == 6);
+	store_db(url, size, score);
+	printf("%s %llu %s\n", url, size, score);
+	free(url);
+	free(score);
+	pclose(fp);
+
 	free(it->path);
 	free(it);
 	free(xcmd);
@@ -58,6 +151,8 @@ void process_one_item(struct item *it, struct barrier *bar) {
 			nit = calloc(1, sizeof(*nit));
 			nit->path = strdup(lpath);
 			deferred[di++] = nit;
+	// Defer queueing work. We don't want to get into deep chains and
+	// workqueue() can itself process part of the queue.
 	//		workqueue(process_one_item, nit, bar);
 		} else {
 			lpath = strstr(linebuf, "s3");
@@ -91,6 +186,8 @@ int main(int argc, char *argv[]) {
 	struct barrier bio_wait;
 	struct item *it;
 
+	setup_db();
+
 	it = calloc(1, sizeof(*it));
 	bzero(&bio_wait, sizeof(bio_wait));
        
@@ -98,4 +195,6 @@ int main(int argc, char *argv[]) {
 
 	process_one_item(it, &bio_wait);
 	bwait(&bio_wait);
+
+	teardown_db();
 }
