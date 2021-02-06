4377aaa252dc200a4660a586a413945196a5b36f
Venkatesh Srinivas <vsrinivas@ops101.org>
2021-02-06 13:49:52 -0800

    Pull in ninja-test; add a basic UT

---
+++ fnv1a_test.cc
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <sys/types.h>
+
+#include "third_party/ninja-test/src/test.h"
+#include "fnv1a.h"
+
+TEST(Fnv1A32Test, FNV1A_32) {
+  char buf[] = "hello, world";
+  uint32_t val = FNV1A_32(buf, sizeof(buf));
+  EXPECT_EQ(0x6b0c59a7, val);
+}
